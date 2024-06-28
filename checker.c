55d9dc6572d65812814d43917c37dd7a0e6c29f6
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-06-28 12:58:42 +0530

    Update checker.c
    added else in pure functions

--- checker.c
+++ checker.c
@@ -5,21 +5,21 @@ int isTempOk(float temperature){
   if(temperature < 0 || temperature > 45) {
     printf("Temperature out of range!\n");
     return 0;
-  }
+  }else
   return 1;
 }
 int isSocOk(float soc){
   if(soc < 20 || soc > 80) {
     printf("State of Charge out of range!\n");
     return 0;
-  }
+  }else
   return 1;
 }
 int isChergeRateOk(float chargeRate){
   if(chargeRate > 0.8) {
     printf("Charge Rate out of range!\n");
     return 0;
-  }
+  }else
   return 1;
 }
 int batteryIsOk(float temperature, float soc, float chargeRate) {
