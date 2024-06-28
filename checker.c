7f486e2f40dc5795d9b729e8b9e5a3c85b68f079
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-06-28 13:10:15 +0530

    Update checker.c
    added more case tests using asserts

--- checker.c
+++ checker.c
@@ -5,28 +5,33 @@ int isTempOk(float temperature){
   if(temperature < 0 || temperature > 45) {
     printf("Temperature out of range!\n");
     return 0;
-  }else
+  }else{
   return 1;
+  }
 }
 int isSocOk(float soc){
   if(soc < 20 || soc > 80) {
     printf("State of Charge out of range!\n");
     return 0;
-  }else
+  }else{
   return 1;
+  }
 }
-int isChergeRateOk(float chargeRate){
+int isChargeRateOk(float chargeRate){
   if(chargeRate > 0.8) {
     printf("Charge Rate out of range!\n");
     return 0;
-  }else
+  }else{
   return 1;
+  }
 }
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-   return isTempOk(temperature) && isSocOk(soc) && isChergeRateOk(chargeRate);
+   return isTempOk(temperature) && isSocOk(soc) && isChargeRateOk(chargeRate);
 }
 
 int main() {
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
+  assert(!batteryIsOk(40, 1, 0));
+  assert(!batteryIsOk(1, 20, 2));
 }
