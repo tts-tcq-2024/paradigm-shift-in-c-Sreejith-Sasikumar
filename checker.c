15266992b9ca16e7a4fbfdff4a93709e3d374a1c
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-06-28 12:46:27 +0530

    Update checker.c #1
    Added 3 separate pure functions

--- checker.c
+++ checker.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
 #include <assert.h>
-
-int batteryIsOk(float temperature, float soc, float chargeRate) {
+bool isTempOk(float temperature){
   if(temperature < 0 || temperature > 45) {
     printf("Temperature out of range!\n");
-    return 0;
-  } else if(soc < 20 || soc > 80) {
-    printf("State of Charge out of range!\n");
-    return 0;
-  } else if(chargeRate > 0.8) {
-    printf("Charge Rate out of range!\n");
-    return 0;
+    return false;
   }
-  return 1;
+  return true;
+}
+bool isSocOk(float soc){
+  if(soc < 20 || soc > 80) {
+    printf("State of Charge out of range!\n");
+    return false;
+  }
+  return true;
+}
+bool isChergeRateOk(float chargeRate){
+  if(chargeRate > 0.8) {
+    printf("Charge Rate out of range!\n");
+    return false;
+  }
+  return true;
+}
+int batteryIsOk(float temperature, float soc, float chargeRate) {
+   return (isTempOk(temperature) && isSocOk(soc) && isChergeRateOk(chargeRate))?1:0;
 }
 
 int main() {
