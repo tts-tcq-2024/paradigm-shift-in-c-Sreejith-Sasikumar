589272b98ffefea7f755e2f25957772d00031347
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-07-27 11:46:09 +0530

    Create boundaryRanges.h

---
+++ boundaryRanges.h
@@ -0,0 +1,9 @@
+typedef struct {
+    int lower_bound;
+    int upper_bound;
+    const char* condition;
+} BoundaryRange;
+
+#define NUM_SOC_RANGES 5
+#define NUM_TEMP_RANGES 4
+#define NUM_CHARGE_RATE_RANGES 4
