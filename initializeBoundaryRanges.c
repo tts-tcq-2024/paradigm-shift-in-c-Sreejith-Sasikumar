e7db2e5e0852926b16d41d0290dbfcf2a6feca92
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-07-27 12:21:21 +0530

    Update initializeBoundaryRanges.c - isValueValid

--- initializeBoundaryRanges.c
+++ initializeBoundaryRanges.c
@@ -1,23 +1,23 @@
 #include "boundaryRanges.h"
 
 BoundaryRange soc_ranges[NUM_SOC_RANGES] = {
-    {0, 20, "LOW_SOC_BREACH"},
-    {21, 24, "LOW_SOC_WARNING"},
-    {25, 75, "NORMAL"},
-    {76, 80, "HIGH_SOC_WARNING"},
-    {81, 100, "HIGH_SOC_BREACH"}
+    {0, 20, "LOW_SOC_BREACH", 0},
+    {21, 24, "LOW_SOC_WARNING", 0},
+    {25, 75, "NORMAL", 1},
+    {76, 80, "HIGH_SOC_WARNING", 0},
+    {81, 100, "HIGH_SOC_BREACH", 0}
 };
 
 BoundaryRange temp_ranges[NUM_TEMP_RANGES] = {
-    {-40, 0, "TEMP_TOO_LOW"},
-    {1, 2.25, "LOW_TEMP_WARNING"},
-    {2.26, 42.74, "NORMAL_TEMP"},
-    {42.75, 45, "HIGH_TEMP_WARNING"},
-    {46, 60, "TEMP_TOO_HIGH"}
+    {-40, 0, "TEMP_TOO_LOW", 0},
+    {1, 2.25, "LOW_TEMP_WARNING", 0},
+    {2.26, 42.74, "NORMAL_TEMP", 1},
+    {42.75, 45, "HIGH_TEMP_WARNING", 0},
+    {46, 60, "TEMP_TOO_HIGH", 0}
 };
 
 BoundaryRange charge_rate_ranges[NUM_CHARGE_RATE_RANGES] = {
-    {0, 0.76, "NORMAL_CHARGE_RATE"},
-    {0.77, 0.8, "HIGH_CHARGE_RATE_WARNING"},
-    {0.81, 1, "OVER_CHARGE_RATE_BREACH"}
+    {0, 0.76, "NORMAL_CHARGE_RATE", 1},
+    {0.77, 0.8, "HIGH_CHARGE_RATE_WARNING", 0},
+    {0.81, 1, "OVER_CHARGE_RATE_BREACH", 0}
 };
