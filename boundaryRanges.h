7096577b28840659eac67c1e4fb7324de3f60376
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-07-27 12:04:28 +0530

    Update boundaryRanges.h

--- boundaryRanges.h
+++ boundaryRanges.h
@@ -5,5 +5,5 @@ typedef struct {
 } BoundaryRange;
 
 #define NUM_SOC_RANGES 5
-#define NUM_TEMP_RANGES 4
-#define NUM_CHARGE_RATE_RANGES 4
+#define NUM_TEMP_RANGES 5
+#define NUM_CHARGE_RATE_RANGES 3
