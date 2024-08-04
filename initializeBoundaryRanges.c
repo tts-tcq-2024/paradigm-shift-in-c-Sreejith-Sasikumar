092ec149ca37a68e13fdb3131e39625a79946c5a
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-08-04 13:59:46 +0530

    Update initializeBoundaryRanges.c

--- initializeBoundaryRanges.c
+++ initializeBoundaryRanges.c
@@ -1,6 +1,6 @@
 #include "boundaryRanges.h"
 
-BoundaryRange soc_ranges[NUM_SOC_RANGES] = {
+BoundaryRange soc_ranges[] = {
     {0, 20, "LOW_SOC_BREACH", 0},
     {21, 24, "LOW_SOC_WARNING", 0},
     {25, 75, "NORMAL", 1},
@@ -8,7 +8,7 @@ BoundaryRange soc_ranges[NUM_SOC_RANGES] = {
     {81, 100, "HIGH_SOC_BREACH", 0}
 };
 
-BoundaryRange temp_ranges[NUM_TEMP_RANGES] = {
+BoundaryRange temp_ranges[] = {
     {-40, 0, "TEMP_TOO_LOW", 0},
     {1, 2.25, "LOW_TEMP_WARNING", 0},
     {2.26, 42.74, "NORMAL_TEMP", 1},
@@ -16,7 +16,7 @@ BoundaryRange temp_ranges[NUM_TEMP_RANGES] = {
     {46, 60, "TEMP_TOO_HIGH", 0}
 };
 
-BoundaryRange charge_rate_ranges[NUM_CHARGE_RATE_RANGES] = {
+BoundaryRange charge_rate_ranges[] = {
     {0, 0.76, "NORMAL_CHARGE_RATE", 1},
     {0.77, 0.8, "HIGH_CHARGE_RATE_WARNING", 0},
     {0.81, 1, "OVER_CHARGE_RATE_BREACH", 0}
