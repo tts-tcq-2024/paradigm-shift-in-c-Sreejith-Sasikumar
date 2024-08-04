9d78429085eb9683939017c798fc2b8df262afd7
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-08-04 15:13:01 +0530

    Update initializeBoundaryRanges.h - warnings switch implementation

--- initializeBoundaryRanges.h
+++ initializeBoundaryRanges.h
@@ -2,3 +2,10 @@
 BoundaryRange soc_ranges[];
 BoundaryRange temp_ranges[];
 BoundaryRange charge_rate_ranges[];
+
+/* 
+  Switch to enable/ disable the warnings
+  TRUE: Enables warnings in soc_ranges, temp_ranges and charge_rate_ranges
+  FALSE: Disable warnings in soc_ranges, temp_ranges and charge_rate_ranges (suppresses warnings if customers don't need early warnings)
+*/
+#define WARNINGS_SWITCH TRUE
