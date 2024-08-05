c5431f816406fac90955626d805b911c42baecb8
Venugopal K <venu.shastri@gmail.com>
2024-08-05 10:24:58 +0530

    Update boundaryRanges.c

--- boundaryRanges.c
+++ boundaryRanges.c
@@ -2,18 +2,18 @@
 #include <assert.h>
 #include "boundaryRanges.h"
 
-void print_condition(const char* parameter_name, const char* condition) {
-    printf("The condition for %s : %s.\n", parameter_name, condition);
+void print_message_console(const char* parameter_name, const char* message) {
+    printf("The Message  for %s : %s.\n", parameter_name, message);
 }
 
 int is_within_range(int value, BoundaryRange range) {
     return value >= range.lower_bound && value <= range.upper_bound;
 }
 
-int get_condition(const char* parameter_name, int value, BoundaryRange ranges[], int num_ranges) {
+int evaluate_parameter(const char* parameter_name, int value, BoundaryRange ranges[], int num_ranges) {
     for (int i = 0; i < num_ranges; i++) {
         if (is_within_range(value, ranges[i])) {
-            print_condition(parameter_name, ranges[i].condition);
+            print_message_console(parameter_name, ranges[i].message);
             return ranges[i].isValueValid;
         }
     }
@@ -21,9 +21,9 @@ int get_condition(const char* parameter_name, int value, BoundaryRange ranges[],
 }
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-  return    get_condition("SOC", soc, soc_ranges, SOC_RANGES_LIMIT) &&
-            get_condition("Temperature", temperature, temp_ranges, TEMP_RANGES_LIMIT) &&
-            get_condition("Charge rate", chargeRate, charge_rate_ranges, CHARGE_RATE_LIMIT);
+  return    evaluate_parameter("SOC", soc, soc_ranges, SOC_RANGES_LIMIT) &&
+            evaluate_parameter("Temperature", temperature, temp_ranges, TEMP_RANGES_LIMIT) &&
+            evaluate_parameter("Charge rate", chargeRate, charge_rate_ranges, CHARGE_RATE_LIMIT);
 }
 
 int main() {
